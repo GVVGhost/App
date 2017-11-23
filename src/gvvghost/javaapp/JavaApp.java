5b03bf1719b9fd6a057e8dd29c2dab9b9cb1ace2
Vadim <gvv.ghost@gmail.com>
2017-11-23 23:40:55 +0200

    Внесено зміни в java файл

--- src/gvvghost/javaapp/JavaApp.java
+++ src/gvvghost/javaapp/JavaApp.java
@@ -3,5 +3,6 @@ package gvvghost.javaapp;
 public class JavaApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+		System.out.println("AIDE is here.");
     }
 }
