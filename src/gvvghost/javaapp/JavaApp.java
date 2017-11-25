08af1cc1485256e1b5b6577d970ad8334edcc5ef
GVVGhost <gvv.ghost@gmail.com>
2017-11-25 16:42:08 +0200

    Внесено зміни в JavaApp.java файл

--- src/gvvghost/javaapp/JavaApp.java
+++ src/gvvghost/javaapp/JavaApp.java
@@ -1,8 +1,37 @@
 package gvvghost.javaapp;
+import java.io.*;
+import java.net.*;
 
-public class JavaApp {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-		System.out.println("AIDE is here.");
+public class JavaApp
+{
+    public static void main(String[] args)
+    {
+        ServerSocket serverSocket = null;
+
+        try
+        {
+            serverSocket = new ServerSocket(5555);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        while(true)
+        {
+            try
+            {
+                Socket s = serverSocket.accept();
+                System.out.println(s.getInetAddress().toString());
+                OutputStream outputStream = s.getOutputStream();
+                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                dataOutputStream.writeUTF("It is the server!\n");
+                dataOutputStream.close();
+                outputStream.close();
+            }
+            catch (IOException | NullPointerException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
